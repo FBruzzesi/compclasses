6d51ac39103ac8fbf81d7f073c512334a69adc67
FBruzzesi <francesco.bruzzesi@edison.it>
2023-01-04 16:15:41 +0100

    added delegatee test

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+import pytest
+
+
+class Foo:
+    """Foo class"""
+
+    a: float = 0.1
+
+    def __init__(self, value: int):
+        """foo init"""
+        self._foo: int = value
+
+    def get_foo(self):
+        """get _foo attribute"""
+        return self.bar
+
+    def hello_from_foo(self, name: str) -> str:
+        """Method with argument"""
+        return f"Hello {name}, this is Bar!"
+
+    def __len__(self) -> int:
+        """Custom len method"""
+        return 42
+
+    # TODO: test staticmethod's, classmethod's, property's
+
+
+class Bar:
+    """Bar class"""
+
+    def __init__(self, foo: Foo, bar_value: int = 123):
+        self.foo = foo
+        self._bar = bar_value
+
+    def get_bar(self):
+        """get _bar attribute"""
+        return self._bar
+
+
+@pytest.fixture
+def foo_cls():
+    """Fixture returning foo class definition"""
+
+    return Foo
+
+
+@pytest.fixture
+def bar_cls():
+    """Fixture returning foo class definition"""
+
+    return Bar
